2758dcb30d1b2481365ce72176c342e33066b808
mukulydv <83510622+mukulydv@users.noreply.github.com>
2023-04-25 12:40:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template , jsonify
+from flask_ngrok import run_with_ngrok
+app = Flask(__name__)
+# run_with_ngrok(app)
+Jobs =[
+    {'id':'Job1', 'title':'Full Stack Developer', 'location':'Amritapuri', 'salary':'Rs.15,00,000', 'mailId':'campus@am.amrita.edu'},
+    {'id':'Job2', 'title':'Backend Developer', 'location':'Coimbatore','salary':'Rs.12,00,000' ,'mailId':'univhq@amrita.edu'},
+    {'id':'Job3', 'title':'ML Engineer', 'location':'Bengaluru', 'salary':'Rs.20,00,000','mailId':'info@blr.amrita.edu'}
+    
+]
+
+@app.route("/")
+def hello_mukul():
+    return render_template('home.html', 
+                           jobs=Jobs,
+                           company_name="Amrita")
+
+@app.route("/api/jobs")
+def get_jobs():
+    return jsonify(jobs=Jobs)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug = True)
+    # app.run()
+
+    
